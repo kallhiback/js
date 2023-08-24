539718b59825b7686bd28e7e1d9c40af9dcef708
kallhiback <130337500+kallhiback@users.noreply.github.com>
2023-08-24 20:33:50 +0600

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,7 @@
+document.getElementById("termux").addEventListener("click", function() {
+    window.location.href = "html/termux.html"
+})
+
+document.getElementById("dark").addEventListener("click", function() {
+    window.location.href = "html/dark.html"
+})
